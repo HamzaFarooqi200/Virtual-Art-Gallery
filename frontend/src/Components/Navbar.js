d957592cb1b44071992918be0da6f6c38f166c72
jamsh <jamshaidmehmood1234@gmail.com>
2024-04-09 16:30:27 +0500

    adding watermark done and also done with the design of login,signuo and forgot password and also rendering specific user picture in navbar also done

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { useLogout } from "../Hooks/useLogout";
@@ -21,11 +21,42 @@ function classNames(...classes) {
 const Navbar = () => {
   const { logOut } = useLogout();
   const { user } = useAuthenticationContextHook();
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const onClickHandler = () => {
     console.log("LOG OUT Clicked");
     logOut();
   };
+
+  useEffect(() => {
+    // Fetch user profile data when the component mounts
+    async function fetchUserData() {
+      setLoading(true);
+      try {
+        const response = await fetch(`/api/userProfile/getUserProfileData?email=${user.email}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        const data = await response.json();
+        //console.log("User data fetched: ", data);
+        setUserData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        setError(error.message);
+        setLoading(false);
+      }
+    }
+
+    if (user) {
+      fetchUserData();
+    }
+  }, [user]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
     <Disclosure as="nav" className="bg-green-400 text-black fixed w-full z-10">
@@ -44,59 +75,57 @@ const Navbar = () => {
               <div className="hidden sm:block sm:ml-10 space-x-4">
                 {user ? (
                   <div>
-                  <Link
-                    to="/discoverArt"
-                    className={classNames(
-                      "text-white",
-                      "mr-2 px-4 py-2 rounded-md text-sm font-medium ",
-                      "fs-4"
-                    )}
-                  >
-                    Discover Art
-                  </Link>
-                <Link
-                  to="/VirtualGallery"
-                  className={classNames(
-                    "text-white",
-                    "mr-2 px-4 py-2 rounded-md text-sm font-medium",
-                    "fs-4"
-                  )}
-                >
-                  Virtual Exhibition 
-                </Link>
-                <Link
-                  to="/uploadArt"
-                  className={classNames(
-                    "text-white",
-                        "mr-2 px-4 py-2 rounded-md text-sm font-medium",
-                        "fs-4"
-                  )}
-                >
-                  Upload Art
-                </Link>
-                <Link
-                  to="/AddCart"
-                  className={classNames(
-                    "text-white",
+                    <Link
+                      to="/discoverArt"
+                      className={classNames(
+                        "text-white",
                         "mr-2 px-4 py-2 rounded-md text-sm font-medium ",
                         "fs-4"
-                  )}
-                >
-                  Your Cart 
-                </Link>
-                <Link
-                  to="/team"
-                  className={classNames(
-                    "text-white",
-                    "mr-2 px-4 py-2 rounded-md text-sm font-medium ",
-                    "fs-4"
-                  )}
-                >
-                  Team
-                </Link>
-
-                </div>
-
+                      )}
+                    >
+                      Discover Art
+                    </Link>
+                    <Link
+                      to="/VirtualGallery"
+                      className={classNames(
+                        "text-white",
+                        "mr-2 px-4 py-2 rounded-md text-sm font-medium",
+                        "fs-4"
+                      )}
+                    >
+                      Virtual Exhibition
+                    </Link>
+                    <Link
+                      to="/uploadArt"
+                      className={classNames(
+                        "text-white",
+                        "mr-2 px-4 py-2 rounded-md text-sm font-medium",
+                        "fs-4"
+                      )}
+                    >
+                      Upload Art
+                    </Link>
+                    <Link
+                      to="/AddCart"
+                      className={classNames(
+                        "text-white",
+                        "mr-2 px-4 py-2 rounded-md text-sm font-medium ",
+                        "fs-4"
+                      )}
+                    >
+                      Your Cart
+                    </Link>
+                    <Link
+                      to="/team"
+                      className={classNames(
+                        "text-white",
+                        "mr-2 px-4 py-2 rounded-md text-sm font-medium ",
+                        "fs-4"
+                      )}
+                    >
+                      Team
+                    </Link>
+                  </div>
                 ) : (
                   <>
                     <Link
@@ -125,67 +154,70 @@ const Navbar = () => {
               </div>
               {user && (
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-circle focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-16 rounded-circle"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
+                  <button
+                    type="button"
+                    className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                   >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/profile"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </Link>
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="ml-3 relative">
+                    <div>
+                      <Menu.Button className="bg-gray-800 flex text-sm rounded-circle focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <span className="sr-only">Open user menu</span>
+                        {userData[0] && userData[0].image && (
+                          <img
+                            className="h-8 w-16 rounded-circle"
+                            src={require(`../uploads/ProfileImage/${userData[0].image}`)}
+                            alt=""
+                          />
                         )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            onClick={onClickHandler}
-                          >
-                            Log out
-                          </Link>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>)}
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="/profile"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Your Profile
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="/"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                              onClick={onClickHandler}
+                            >
+                              Log out
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              )}
             </div>
           </div>
 
